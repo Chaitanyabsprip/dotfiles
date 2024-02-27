6c8b3adfb23bfc68485240b8a4a42dc312f50497
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-02-28 02:38:54 +0530

    refactor(cdpath.sh): remove unnecessary paths

--- zsh/conf.d/cdpath.sh
+++ zsh/conf.d/cdpath.sh
@@ -12,11 +12,7 @@ cdpath_add() {
 CDPATH="."
 cdpath_add $HOME/dotfiles
 cdpath_add $HOME/.config
-cdpath_add $HOME/Programs
+cdpath_add $HOME/programs
 cdpath_add $HOME/projects
-cdpath_add $HOME/projects/apps
-cdpath_add $HOME/projects/apps/yocket
-cdpath_add $HOME/projects/languages
-cdpath_add $HOME/projects/forks
 cdpath_add $HOME/.local/share/nvim/lazy
 unfunction cdpath_add
