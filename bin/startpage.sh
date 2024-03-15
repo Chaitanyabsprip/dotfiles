c7e6265b095b5436e5cb5e56b5ffbf9433a91403
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-03-16 01:02:53 +0530

    fix: use only python to serve startpage

--- bin/startpage.sh
+++ bin/startpage.sh
@@ -4,22 +4,24 @@ _have() { type "$1" >/dev/null 2>&1; }
 
 PORT=8910
 
+cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}"
+log_file="$cache_dir/minwall/server.log"
+
 setup() {
-	echo "Hai bhai" >~/.temp
-	cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}"
-	log_file="$cache_dir/minwall/server.log"
 	cd ~/.config/minwall/ || exit 1
 	[ ! -d "$cache_dir/minwall" ] && mkdir "$cache_dir/minwall"
 	[ ! -f "$log_file" ] && touch "$log_file"
 }
 
-if _have browser-sync &&
-	! { pgrep -f -l browser-sync | grep -q $PORT; }; then
-	setup &&
-		nohup browser-sync --host 127.0.0.1 --port $PORT >/dev/null 2>&1 &
+started="false"
+if _have python3 && ! { pgrep -fa http.server | grep -q $PORT; }; then
+	started="python3 server"
+	setup
+	nohup /usr/bin/python3 -m http.server $PORT >>"$log_file" 2>&1 &
 fi
 
-if _have http-server && ! { pgrep -fl http-server | grep -q $PORT; }; then
-	setup &&
-		nohup http-server -p $PORT >"$log_file" &
+if [ "$started" = "false" ]; then
+	echo "Found neither browser-sync nor http-server or already running" >>"$log_file" 2>&1
+else
+	echo "Started $started ($(pgrep -fa http.server))" >>"$log_file" 2>&1
 fi
