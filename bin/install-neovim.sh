148387ae6b364ed61db6300741312f5fe64cee73
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-03-28 00:21:13 +0530

    fix: add error messages to install scripts

--- bin/install-neovim.sh
+++ bin/install-neovim.sh
@@ -4,7 +4,6 @@ _depends() { have "$1" ||
 	{ echo "${0##*/} depends on $1, please install and try again." && exit 1; }; }
 
 prereqs() {
-	runas="$1"
 	case "$(uname)" in
 	Darwin)
 		_depends brew
@@ -18,9 +17,9 @@ prereqs() {
 		case "$distro" in
 		alpine) apk add build-base cmake coreutils curl unzip gettext-tiny-dev ;;
 			# CentOS / RHEL / Fedora -- sometime later
-		arch) $runas pacman -S base-devel cmake unzip ninja curl ;;
-		opensuse*) $runas zypper install ninja cmake gcc-c++ gettext-tools curl ;;
-		ubuntu) $runas apt-get -y --no-install-recommends install ninja-build gettext cmake unzip curl make ;;
+		arch) sudo pacman -S base-devel cmake unzip ninja curl ;;
+		opensuse*) sudo zypper install ninja cmake gcc-c++ gettext-tools curl ;;
+		ubuntu) sudo apt-get -y --no-install-recommends install ninja-build gettext cmake unzip curl make ;;
 		esac
 		;;
 	*) echo "${RED}Unknown or unsupported OS${NC}" && return ;;
@@ -29,17 +28,15 @@ prereqs() {
 
 install_neovim() {
 	have nvim && exit
-	runas="$([ ! "$(whoami)" = root ] && echo "sudo")"
-	[ -n "$runas" ] && _depends sudo
-	prereqs "$runas"
+	_depends sudo
+	prereqs
 	mkdir ~/programs && {
 		cd ~/programs || { echo "${RED}Failed to install neovim${NC}" && return; }
 	}
 	git clone --depth 1 --branch nightly https://github.com/neovim/neovim
 	cd neovim && make CMAKE_BUILD_TYPE=Release
 	# shellcheck disable=2086
-	[ -n "$runas" ] && have $runas || echo "Requires $runas to install"
-	$runas make install
+	sudo make install
 	rm -r ~/programs/neovim || true
 	echo "${GREEN}neovim successfully installed${NC}"
 }
