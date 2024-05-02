78f87be72a22c9b996317586a6e9e3b19b443679
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-05-02 20:33:22 +0530

    fix(tmux): handle clashing session names for different dirs better

--- tmux/bin/tmux-sessionizer.sh
+++ tmux/bin/tmux-sessionizer.sh
@@ -1,36 +1,67 @@
 #!/bin/sh
 
-_depends() { type "$1" >/dev/null 2>&1 ||
-	{ echo "${0##*/} depends on $1, please install and try again." &&
-		exit 1; }; }
+sessionizer() {
+	_depends() { type "$1" >/dev/null 2>&1 ||
+		{ echo "${0##*/} depends on $1, please install and try again." &&
+			exit 1; }; }
 
-_depends fzf-tmux
+	_depends fzf-tmux
 
-if [ $# -eq 1 ]; then
-	selected=$1
-else
-	selected=$(
-		workdirs | sed "s,$HOME/\.\?,," | fzf-tmux -p --border \
-			--border-label=' Sessionizer ' \
-			--border-label-pos=6:bottom
-	)
-fi
+	main() {
+		newpath="$(select_path "$@")"
+		[ -z "$newpath" ] && exit 0
+		session_name="$(basename "$newpath")"
 
-if [ -z "$selected" ]; then
-	exit 0
-fi
+		if is_tmux_running; then
+			session_name="$(basename "$newpath")"
+			tmux new-session -s "$session_name" -c "$newpath"
+			exit 0
+		fi
 
-selected="$(workdirs | grep -w "$selected$")"
-selected_name=$(basename "$selected" | tr . _)
-tmux_running=$(pgrep tmux)
+		if session_exists "$newpath"; then
+			tmux switch-client -t "$(list_sessions | grep -w "$newpath" | cut -d '=' -f 1)"
+			exit 0
+		fi
 
-if [ -z "$TMUX" ] && [ -z "$tmux_running" ]; then
-	tmux new-session -s "$selected_name" -c "$selected"
-	exit 0
-fi
+		if session_exists "$session_name"; then
+			oldpath="$(list_sessions | grep -w "$session_name" | cut -d '=' -f 2)"
+			old_session_new_name="$(basename "$(dirname "$oldpath")")/$(basename "$oldpath")"
+			tmux rename-session -t "$session_name" "$old_session_new_name"
+			session_name="$(basename "$(dirname "$newpath")")/$(basename "$newpath")"
+		fi
 
-if ! tmux has-session -t="$selected_name" 2>/dev/null; then
-	tmux new-session -ds "$selected_name" -c "$selected"
-fi
+		tmux new-session -ds "$session_name" -c "$newpath"
+		tmux switch-client -t "$session_name"
+	}
 
-tmux switch-client -t "$selected_name"
+	select_path() {
+		if [ -n "$1" ]; then
+			newpath=$1
+		else
+			newpath=$(
+				workdirs | sed "s,$HOME/\.\?,," | fzf-tmux -p --border \
+					--border-label=' Sessionizer ' \
+					--border-label-pos=6:bottom
+			)
+		fi
+		[ -z "$newpath" ] && exit 0
+		workdirs | grep -w "$newpath"
+	}
+
+	is_tmux_running() {
+		tmux_running=$(pgrep tmux)
+		[ -z "$TMUX" ] && [ -z "$tmux_running" ]
+	}
+
+	session_exists() {
+		list_sessions | grep -wq "$1"
+	}
+
+	list_sessions() {
+		tmux ls -F '#{session_name}=#{session_path}'
+	}
+
+	main "$@"
+}
+
+sessionizer "$@"
