bca149b908cb868b7812671f81a1525a0c76caa7
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-06-09 04:13:18 +0530

    fix(zsh): don't show file/dirs for make completion

--- zsh/conf.d/completions.sh
+++ zsh/conf.d/completions.sh
@@ -2,4 +2,5 @@
 
 autoload -U compinit && compinit # Load and initialise completion system.
 zstyle ':completion:*' menu select
+zstyle ':completion:*:*:make:*' tag-order 'targets variables'
 _comp_options+=(globdots) # Include hidden files in completions.
