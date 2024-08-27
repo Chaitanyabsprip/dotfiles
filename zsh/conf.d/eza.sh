5967b0ec1ae940678a62168adbd4044b5cb723c2
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-08-27 20:17:43 +0530

    feat(eza): list files in column

--- zsh/conf.d/eza.sh
+++ zsh/conf.d/eza.sh
@@ -8,11 +8,11 @@ _have eza || return
 # Use the --git flag if the installed version of eza supports git
 # Related to https://github.com/ogham/exa/issues/978
 if eza --version | grep -q '+git'; then
-	alias ls='eza -F --group-directories-first --icons --git'
+	alias ls='eza -F --group-directories-first --icons --git -1'
 	alias ll='ls -lhF --git'
 	alias la='ll -a'
 else
-	alias ls='eza --group-directories-first --icons'
+	alias ls='eza --group-directories-first --icons -1'
 	alias ll='ls -lhF'
 	alias la='ll -a'
 fi
