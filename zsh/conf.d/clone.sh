dd7a03202fdb7c74966799b18ffdd2663313bf54
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-10-19 13:41:12 +0530

    feat(zsh): clone in /home/chaitanya/projects for my repos

--- zsh/conf.d/clone.sh
+++ zsh/conf.d/clone.sh
@@ -5,14 +5,17 @@ clone() {
 	shift
 	repo="$(echo "$repo" | sed 's,\(^https://github.com/|^git@github.com:\),,')"
 	case "$repo" in
-	*/*) user=$(echo "$repo" | cut -d'/' -f1) ;;
+	*/*) user="$(echo "$repo" | cut -d'/' -f1)" ;;
 	*) user="${GITUSER:-Chaitanyabsprip}" ;;
 	esac
+	if [ "$user" = "${GITUSER:-Chaitanyabsprip}" ]; then
+		user=""
+	fi
 	name=$(echo "$repo" | sed 's|.*/||')
 	userd="${PROJECTS:-$HOME/projects}/$user"
-	localPath="$userd/$name"
+	localPath="$(echo "$userd/$name" | sed 's|/$||')"
 	[ -d "$localPath" ] && cd "$localPath" && return
-	: $(mkdir -p "$userd")
+	: "$(mkdir -p "$userd")"
 	cd "$userd" || :
 	echo gh repo clone "$user/$name" -- --recurse-submodule "$@"
 	gh repo clone "$user/$name" -- --recurse-submodule "$@"
