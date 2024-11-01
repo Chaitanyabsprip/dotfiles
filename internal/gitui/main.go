278aee690ce0f49ef4c2380cbc99f8874b7b4a24
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-11-02 03:07:14 +0530

    refactor: add gitui subcmd to dot cli

---
+++ internal/gitui/main.go
@@ -0,0 +1,39 @@
+package gitui
+
+import (
+	"embed"
+
+	e "github.com/Chaitanyabsprip/dot/internal/core/embed"
+	bonzai "github.com/rwxrob/bonzai/pkg"
+	"github.com/rwxrob/bonzai/pkg/core/comp"
+
+	"github.com/Chaitanyabsprip/dot/internal/core/oscfg"
+)
+
+//go:embed gitui
+var embedFs embed.FS
+
+var Cmd = &bonzai.Cmd{
+	Name:  `gitui`,
+	Usage: `gitui <command>`,
+	Short: `gitui is a utility to manage gitui configuration`,
+	Comp:  comp.Cmds,
+	Cmds:  []*bonzai.Cmd{setupCmd},
+	Init: func(x *bonzai.Cmd, args ...string) error {
+		for _, cmd := range x.Cmds {
+			cmd.Caller = x
+		}
+		return nil
+	},
+}
+
+var setupCmd = &bonzai.Cmd{
+	Name:  `setup`,
+	Usage: `setup <opts>`,
+	Opts:  `slim|quik|full`,
+	Short: `Setup gitui`,
+	Comp:  comp.Opts,
+	Call: func(x *bonzai.Cmd, args ...string) error {
+		return e.SetupAll(embedFs, "gitui", oscfg.ConfigDir())
+	},
+}
