f875b9bacd92ddd4e67dca794acef730aad1564f
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-11-12 21:04:31 +0530

    docs(distro): add possible values for Name method

--- x/distro/distro.go
+++ x/distro/distro.go
@@ -1,20 +1,45 @@
 package distro
 
 import (
-	"io"
-	"log"
 	"strings"
 
-	"github.com/Chaitanyabsprip/dot/x/have"
 	"github.com/rwxrob/bonzai/futil"
 	"github.com/rwxrob/bonzai/run"
+
+	"github.com/Chaitanyabsprip/dot/x/have"
 )
 
+// Name returns the name of the distro for UNIX-like systems.
+// Possible values (not exhaustive):
+//
+// - Debian based distros:
+//   - Debian GNU/Linux
+//   - Ubuntu
+//   - Linux Mint
+//   - Pop!_OS
+//   - Kali GNU/Linux
+//
+// - Red Hat based distros:
+//   - Fedora Linux
+//   - CentOS Linux
+//   - Rocky Linux
+//   - AlmaLinux
+//
+// - Arch based distros:
+//   - Arch Linux
+//   - Manjaro Linux
+//   - EndeavourOS
+//
+// - SUSE based distros:
+//   - openSUSE Leap
+//   - openSUSE Tumbleweed
+//
+// - Other distros:
+//   - Alpine Linux
 func Name() string {
-	log.SetOutput(io.Discard)
 	osReleaseFile := "/etc/os-release"
 	if futil.Exists(osReleaseFile) {
-		line, err := futil.FindString(osReleaseFile, "NAME=.*")
+		line, err := futil.FindString(osReleaseFile, "^NAME=.*")
 		if err != nil {
 			return ""
 		}
@@ -29,3 +54,7 @@ func Name() string {
 		return strings.TrimSpace(run.Out("uname -s"))
 	}
 }
+
+// Version returns the version of the distro
+// TODO(chaitanya): Unimplemented
+func Version() string { return "" }
