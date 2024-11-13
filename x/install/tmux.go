8863ee6483cf5085a0114dd3b6d8e723fc672f14
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-11-14 01:51:07 +0530

    fix(tmux): correctly install tmux

--- x/install/tmux.go
+++ x/install/tmux.go
@@ -14,27 +14,27 @@ import (
 var TmuxCmd = &bonzai.Cmd{
 	Name:  `tmux`,
 	Alias: `i`,
-	Do:  func(_ *bonzai.Cmd, _ ...string) error { return Tmux() },
+	Do:    func(_ *bonzai.Cmd, _ ...string) error { return Tmux() },
 }
 
 func Tmux() error {
-	if ok, _ := have.Executable("tmux"); ok {
-		fmt.Println("tmux is already installed")
+	if ok, _ := have.Executable(`tmux`); ok {
+		fmt.Println(`tmux is already installed`)
 		return nil
 	}
 	switch distro.Name() {
-	case "Arch Linux":
-		return WithRoot("pacman", "-S", "tmux")
-	case "Ubuntu", "Debian GNU/Linux":
-		return WithRoot("apt-get", "install", "tmux")
-	case "Fedora Linux":
-		return run.SysExec("dnf", "install", "tmux", "-y")
-	case "Darwin":
-		return run.SysExec("brew", "install", "tmux")
+	case `Arch Linux`:
+		return WithRoot(`pacman`, `-S`, `tmux`)
+	case `Ubuntu`, `Debian GNU/Linux`:
+		return WithRoot(`apt-get`, `install`, `-y`, `tmux`)
+	case `Fedora Linux`:
+		return run.SysExec(`dnf`, `install`, `tmux`, `-y`)
+	case `Darwin`:
+		return run.SysExec(`brew`, `install`, `tmux`)
 	default:
 		fmt.Fprintln(
 			os.Stderr,
-			"Unsupported operating system. Please install tmux manually.",
+			`Unsupported operating system. Please install tmux manually.`,
 		)
 	}
 	return nil
