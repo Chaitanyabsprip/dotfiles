7619898c374efbf111e555bb26da3c76eaea01b1
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-11-16 14:07:27 +0530

    feat(zsh): add bash comp and other completions

--- zsh/conf.d/completions.sh
+++ zsh/conf.d/completions.sh
@@ -1,8 +1,11 @@
 #!/bin/zsh
 
-autoload -U compinit && compinit # Load and initialise completion system.
+autoload -U compinit && compinit         # Load and initialise completion system.
+autoload -U bashcompinit && bashcompinit # Load bash completions.
 zstyle ':completion:*' menu select
 zstyle ':completion:*:*:make:*' tag-order 'targets variables'
 _comp_options+=(globdots) # Include hidden files in completions.
 
+complete -C dot dot
 complete -C x x
+complete -C kimono kimono
